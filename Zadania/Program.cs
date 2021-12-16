9a77825f77035644fb0e1059718af9e6fdc7dbcd
Czarkoo <kacper.czajer03@gmail.com>
2021-12-16 17:45:15 +0100

    ready

--- Zadania/Program.cs
+++ Zadania/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Zadania
+namespace ppk_main
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.WriteLine("liczba 1");
+            int liczba_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("liczba 2");
+            int liczba_2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" + dla dodawania, - dla odejmowani,* dla mnożenia, / dla dzielenia");
+            char liczba = Console.ReadLine()[0];
+            switch (liczba)
+            {
+                case '+':                  
+                    Console.WriteLine(liczba_1 + liczba_2);
+                    break;
+
+                case '-':
+                    Console.WriteLine(liczba_1 - liczba_2);                   
+                    break;
+                case '*':
+                    Console.WriteLine(liczba_1 * liczba_2);
+                    break;
+                case '/':
+
+                    if (liczba_1 == 0 || liczba_2 == 0)
+                    {
+                        Console.WriteLine("Nie można dzielić przez 0");
+                    }
+                    else
+                        Console.WriteLine(liczba_1 * liczba_2);
+                    break;
+
+                default:                    
+                    Console.WriteLine("Nie obsługuję takiego znaku!");
+                    break;
+            }
         }
     }
 }
