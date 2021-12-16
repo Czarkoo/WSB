d3154b412bef3ec47cf4287c68784660f4a010c6
Czarkoo <kacper.czajer03@gmail.com>
2021-12-16 17:37:26 +0100

    ready

--- Zadania/Program.cs
+++ Zadania/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Zadania
+namespace ppk_main
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.WriteLine("liczba 1");
+            int liczba_1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("liczba  2");
+            int liczba_2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Wprowadz '+' dla dodawania lub '-' dla odejmowani");
+            char znak = Console.ReadLine()[0];
+
+
+            if (znak == '-')
+            {
+
+                Console.WriteLine(liczba_1 - liczba_2);
+
+            }
+            else if (znak == '+')
+            {
+                Console.WriteLine(liczba_1 + liczba_2);
+            }
+            else
+            {
+                Console.WriteLine("nie obsługuję tego znaku");
+            }
         }
     }
 }
